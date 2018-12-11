7919b1808c8580de9089e15a195ce675dd21c499
chunchaolee <chunchaolee@gmail.com>
2018-12-11 21:36:23 +0800

    Generate role model and roles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_124618) do
+ActiveRecord::Schema.define(version: 2018_12_11_132827) do
+
+  create_table "roles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
